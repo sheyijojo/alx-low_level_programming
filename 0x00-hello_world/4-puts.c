4ffdb282bc257883ea4935127fb0205e35028cd0
Sheyi Gaji <seezie@gmail.com>
2022-06-30 18:56:16 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 
-/*
- *main- starting poiint, prints the phrase "Programming is like ..."
- *Return: 0 as the stopping point
- */
 
+
+/**
+ *
+ *  * main - prints the phrase "Programming is like building a multilingual puzzle
+ *
+ *   * Return: 0 if exited properly, non-zero otherwise
+ *
+ *    */
 
 int main(void)
+
 {
 
-	
- 	puts("Programming is like building a multilingual puzzle \n")
-	return(0);
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
 
 }
