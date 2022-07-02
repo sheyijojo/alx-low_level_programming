11205ea4fae723eb084fbaa7c0b3e9aaec0293e6
Sheyi Gaji <seezie@gmail.com>
2022-07-02 11:16:52 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
+
+
 /**
- * main- entry point
- * Description: prints all nums of base16 fflwd by nwline
- * Return: Always 0 (success)
- * @void: no argument
+ * main- This is the entry block to the code
+ * Description: prints hexadecimals in lower case wt a new linw
+ * Return: returns 0 (success!)
+ * @void: no arguments
  */
+
+
+
 int main(void)
 {
 	int a;
 	char b;
 
-	for (a = 0; a <= 9; a++)
-	putchar(a + '0' );
+	for (a = 0; a < 10; a++)
+	putchar(a + '0');
 	for (b = 'a'; b <= 'f'; b++)
 	putchar(b);
 	putchar('\n');
 	return (0);
-
 }
