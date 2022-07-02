22bdc01372320f7aa3e207e653ca10039b8204ce
Sheyi Gaji <seezie@gmail.com>
2022-07-01 23:00:02 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main- entry
+ * Description: prints all single digits of bs10, from 0 ffl by new line
+ * Return: returns 0 (success!)
+ * @void: no arguments
+ */
+int main(void)
+{
+
+	int num  = '1';
+
+	for (num = '1'; num <= 9; num++)
+	putchar(num);
+	putchar('\n');
+
+
+	return (0);
+
+}
+
