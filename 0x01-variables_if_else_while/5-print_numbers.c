2f97f3e376d546ddab5ae210b62aaec0d718de50
Sheyi Gaji <seezie@gmail.com>
2022-07-01 21:57:03 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- This is the entry block to the code
+ * Description: prns all single digit numbers of base10, fm 0 nd a new line
+ * Return: returns 0 (success!)
+ * @void: no arguments
+ **/
+int main(void)
+{
+
+	int a;
+
+	for (a = 0; a <= 10; a++)
+		printf("%d\n", a);
+	return (0);
+}
