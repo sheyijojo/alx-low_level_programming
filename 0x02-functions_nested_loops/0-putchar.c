0684b1f909c6b52f9b46be02cfe7029866a3c3bb
Sheyi Gaji <seezie@gmail.com>
2022-07-05 21:24:36 -0700

    redit main.py

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,21 @@
 #include "main.h"
+/**
+ * main- This is the entry block to the code
+ * Description: prints _putchae followed by a new line
+ * Return: returns 0 (success!)
+ */
+int main(void)
+{
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
