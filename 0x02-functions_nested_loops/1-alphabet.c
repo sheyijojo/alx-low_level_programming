66bbd7b7d2ac19fbd1fb76a21bcd7b1534bb8dc1
Sheyi Gaji <seezie@gmail.com>
2022-07-05 21:41:23 -0700

    . I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet gam

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main- This is the entry block to the code
+ * Description: func to print abc
+ * Return: returns 0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+
+	}
+
+	_putchar('\n');
+
+}
+
