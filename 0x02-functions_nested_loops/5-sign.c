6ea30eca4cb5d1fd75b47a12593d1b22017be98b
Sheyi Gaji <seezie@gmail.com>
2022-07-06 05:01:19 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of a number
+ * @n: argument to be checked
+ * Return: 1 for positive num, -1 for negative num and 0 for zero.
+ */
+int print_sign(int n)
+{
+
+	if (n > 0)
+
+	{
+		_putchar('+');
+
+		return (1);
+	}
+
+	else if (n == 0)
+
+	{
+		_putchar(('0');
+
+		return (0);
+	}
+
+	else
+
+	{
+		_putchar('-');
+		return (-1);
+
+	}
+}
