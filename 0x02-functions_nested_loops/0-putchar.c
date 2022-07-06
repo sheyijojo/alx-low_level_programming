235e41be64065acbf7f4fe49fe407700842751e4
Sheyi Gaji <seezie@gmail.com>
2022-07-05 17:57:26 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * main- This is the entry block to the code
+ * Description: prints _putchar followed by a new line
+ * Return: returns 0 (success!)
+*/
+int main(void)
+
+{
+
+		char c[8] = "_putchar";
+
+		int i = 0;
+
+		while (i < 8)
+
+		{
+			_putchar(c[i]);
+			i++;
+
+		}
+
+		_putchar('\n');
+	return (0);
+}
