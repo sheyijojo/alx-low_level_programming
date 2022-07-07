fab7c7e90562937ee3e4efeff9bf5b78b4676550
Sheyi Gaji <seezie@gmail.com>
2022-07-07 04:46:19 -0700

    Test the function if positive with 0 as input

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * main - tests function that prints if integer is positive or negative
+ * positive_or_negative: function to be imported to test code
  * Return: 0
  */
 int main(void)
