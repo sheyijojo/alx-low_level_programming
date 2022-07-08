d41fa84c33bf5df72515455b5cc969e1f85b0c8d
Sheyi Gaji <seezie@gmail.com>
2022-07-07 19:38:23 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,23 +8,27 @@
  */
 int main(void)
 {
+
 	int i;
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0)
-			printf("FizzBuzz";
-		else if (i % 3 == 0)
-			printf("Fizz");
-		else if (i % 5 == 0)
-			printf("Buzz");
-		else
-			print("%i", i);
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf(" Fizz");
 
-		if (i != 100)
-			print(" ");
-
+		} else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Buzz");
+		} else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		} else if (i == 1)
+		{
+			printf("%d", i);
+		} else
+		{
+			printf(" %d", i);
+		}
 	}
 	printf("\n");
-	return (0);
-}
