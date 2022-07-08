bac523c03cd1999126304c9705cf75f7cb1c5daf
Sheyi Gaji <seezie@gmail.com>
2022-07-07 19:51:27 -0700

    Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,7 +8,7 @@ void print_triangle(int size)
 	if (size <= 0)
 	{
 		_putchar('\n);
-	}else
+	} else
 
 	{
 		int i, j;
@@ -17,10 +17,12 @@ void print_triangle(int size)
 		{
 			for (j = i; j < size; j++)
 			{
-				_putchar(' ');
+				_putchar('');
 			}
 
+
 			for (j = 1; j <= i; j++)
+
 			{
 				_putchar('#');
 			}
