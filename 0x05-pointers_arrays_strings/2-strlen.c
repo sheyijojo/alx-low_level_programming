9a55cc03e450c4fe8c2a865f9bba5d583dd41022
Sheyi Gaji <seezie@gmail.com>
2022-07-11 16:36:09 -0700

    This report, by its very length, defends itself against the risk of being readThis report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - returns the length of d string
+ * @s: input string
+ * @n: input int
+ * Return: void
+ */
+int _strlen(char *s);
+{
+	int a = 0;
+
+	while (*(s + a))
+		a++;
+
+	return (a);
+}
+
