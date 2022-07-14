e640bdd951eb080681cecf310d6104438e3a3532
Sheyi Gaji <seezie@gmail.com>
2022-07-14 12:14:04 -0700

    Numbers have life; they're not just symbols on paper

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -16,31 +16,22 @@ void print_number(int n)
 	}
 
 	else
-
 	{
 
 		a = n;
-
 	}
 
 	b = a;
 	c = 1;
 
 	while (b > 9)
-
 	{
-
 		b /= 10;
-
 		c *= 10;
 	}
-
 	for (; c >= 1; c /= 10)
-
 	{
-
 		_putchar(((a / c) % 10) + 48);
 
 	}
-
 }
