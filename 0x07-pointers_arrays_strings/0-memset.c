f61825523af9d1da122a9ed7560d37bd96d1dc6c
Sheyi Gaji <seezie@gmail.com>
2022-07-18 17:52:59 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,27 +6,14 @@
  *@n: bytes of the memory
  *Return: dest
  */
-
-
-
 char *_memset(char *s, char b, unsigned int n)
 {
-
 	unsigned int i;
 
-
 	for (i = 0; i < n; i++)
-
 	{
-
 		*(s + i) = b; /*add 1 position s*/
-
-
-
 	}
 
-
-
 	return (s);
-
 }
