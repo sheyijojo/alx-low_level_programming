106bdef0c655d9bc8ca0d62807679316cf5c635e
Sheyi Gaji <seezie@gmail.com>
2022-07-23 00:37:02 -0700

    modified script

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-gcc -c *.c
-ar rcs liball.a *.o
-/* could decide to add - Wall -Wesxtra -Werroe -pedantic as errors
-*/
+gcc -Wall -Wextra -Werror -pedantic -c *.c
+ar rc liball.a *.o
+
