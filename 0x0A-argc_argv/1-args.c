b1cf915482d328aa3549330e6429c5348f3ba1a9
Sheyi Gaji <seezie@gmail.com>
2022-07-24 10:51:13 -0700

    remodied to remove __attribute__((unused)))

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include<stdio.h>
 /**
- * main- prints the number of arguments passed into it.
- * @agrc: num of cmd arguments.argv is an array of char* that has argc elements
- * @agrv: array that contain prog cmd arguments
- * Return: 0(success)
+ * main - prints the number of arguments passed into it.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
 int main(int argc, char **argv)
 {
