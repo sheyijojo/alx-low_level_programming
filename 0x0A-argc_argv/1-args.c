761c15d2aa9171d16c970496f60ed75eafb967bb
Sheyi Gaji <seezie@gmail.com>
2022-07-24 10:47:36 -0700

    remodied this files

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,13 +2,18 @@
 #include<stdio.h>
 /**
  * main- prints the number of arguments passed into it.
- * @agrc:num of cmd arguments.argv is an array of char* that has argc elements
+ * @agrc: num of cmd arguments.argv is an array of char* that has argc elements
  * @agrv: array that contain prog cmd arguments
  * Return: 0(success)
  */
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char **argv)
 {
-	printf("%d\n", argc - 1);
+	int i;
+
+	for (i = 0; i < argc; i++)
+		(**argv)++;
+
+	printf("%d\n", i - 1);
 
 	return (0);
 }
